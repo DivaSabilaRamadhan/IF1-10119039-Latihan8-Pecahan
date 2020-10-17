7b2ee05fba6b5802ccbcc1162fa79aac96a1b23d
diky <diky@diky-PC>
2020-10-17 13:53:20 +0700

    Upload Project

---
+++ src/Pecahan.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author
+ * NAMA  : Diva Sabila Ramadhan
+ * KELAS : IF1
+ * NIM   : 10119309
+ * Deskripsi Program : Program Pecahan 
+ */
+public class Pecahan {
+
+    
+    public static void main(String[] args) {
+        float f;
+        double d;
+        f = 1234567890.123456789F;
+        d = 1_234_567_890.123456789;
+        System.out.println("Float : " + f);
+        System.out.println("Double : " + d);
+    }
+    
+}
